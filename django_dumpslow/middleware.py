0bddbf9b003a21bf54f7e4a28a72a6e83baed469
Chris Lamb <lamby@debian.org>
2009-03-01 00:10:44 +0000

    Add middleware and signals.

---
+++ django_dumpslow/middleware.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#
+# django-dumpslow -- Django application to log and summarize slow requests
+#                    <http://chris-lamb.co.uk/projects/django-dumpslow>
+#
+# Copyright © 2009 Chris Lamb <chris@chris-lamb.co.uk>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+
+import time
+import logging
+
+from django.conf import settings
+
+from django_dumpslow.signals import long_request
+
+class LogLongRequestMiddleware(object):
+    def process_request(self, request):
+        self.start_time = time.time()
+
+    def process_response(self, request, response):
+        time_taken = time.time() - self.start_time
+
+        try:
+            max_time = settings.LONG_REQUEST_TIME
+        except AttributeError:
+            max_time = 1
+
+        if time_taken > max_time:
+            url = request.META['PATH_INFO']
+
+            log = logging.getLogger('dumpslow')
+            log.warning('Long request - %.3fs %s', time_taken, url)
+
+            long_request.send(sender=request, url=url, time_taken=time_taken)
+
+        return response
---
+++ django_dumpslow/signals.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# django-dumpslow -- Django application to log and summarize slow requests
+#                    <http://chris-lamb.co.uk/projects/django-dumpslow>
+#
+# Copyright © 2009 Chris Lamb <chris@chris-lamb.co.uk>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+
+from django.core.signals import Signal
+
+long_request = Signal(providing_args=('url', 'time_taken'))
