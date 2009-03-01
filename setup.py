f4f4e694d1aa4b200d3c36f337f530a22668e1ee
Chris Lamb <lamby@debian.org>
2009-03-01 00:11:19 +0000

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# django-dumpslow -- Django application to log and summarize slow requests
+#                    <http://chris-lamb.co.uk/projects/django-dumpslow>
+#
+# Copyright © 2009 Chris Lamb <chris@chris-lamb.co.uk>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+
+setup_args = dict(
+    name='django_dumpslow',
+    license='BSD',
+    homepage='http://chris-lamb.co.uk/projects/django-dumpslow',
+    packages=(
+        'django_dumpslow',
+        'django_dumpslow.management',
+        'django_dumpslow.management.commands',
+    ),
+    author='Chris Lamb',
+    author_email='chris@chris-lamb.co.uk',
+)
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(**setup_args)
