f35f90e5c5e0ad9e4503f11a9c04bc6f203874a0
Chris Lamb <lamby@debian.org>
2009-07-06 12:29:09 +0100

    Add example of class-based views.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- django_dumpslow_example/example/urls.py
+++ django_dumpslow_example/example/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls.defaults import *
 
+from .views import slow2
+
 urlpatterns = patterns('example.views',
    url(r'^$', 'index', name='index'),
    url(r'^slow$', 'slow', name='slow'),
+   url(r'^slow2$', slow2(), name='slow2'),
 )
--- django_dumpslow_example/example/views.py
+++ django_dumpslow_example/example/views.py
@@ -9,3 +9,8 @@ def index(request):
 def slow(request):
     time.sleep(2)
     return HttpResponse('This page should have taken >=2s to render.')
+
+class slow2(object):
+    def __call__(self, request):
+        time.sleep(5)
+        return HttpResponse('This page should have taken >=5s to render.')
