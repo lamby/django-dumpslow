479ee109eb0980dff919fe60ec98d88099001b16
Chris Lamb <lamby@debian.org>
2010-07-05 17:30:08 +0100

    Remove signals from example.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- django_dumpslow_example/example/models.py
+++
@@ -1,6 +0,0 @@
-from django_dumpslow.signals import long_request
-
-def long_request_cb(sender, time_taken, url, request, response, **kwargs):
-    print "Slow request detected on %s %.2fs (via signal)" % (url, time_taken)
-
-long_request.connect(long_request_cb)
